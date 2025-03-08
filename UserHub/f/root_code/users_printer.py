73d526aaf5f326bf628cb8fd7ee74a4a71d38906
Alex <alex12141327@163.com>
2025-03-08 19:44:08 +0800

    Delete UserHub directory

--- UserHub/f/root_code/users_printer.py
+++
@@ -1,9 +0,0 @@
-def users_printer(user_con,root_password):
-    password_in = input("enter root password:")
-    if user_con == 2 and password_in == root_password:
-        with open('E:\\code\\python_code\\UserHub\\usermessage.txt','r') as file:
-            lines = file.readlines()
-            for line in lines:
-                print (f"===========users inpormatiopn=========\n{line.strip()}")
-
-
